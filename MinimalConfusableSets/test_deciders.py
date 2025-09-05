e19063fe18d480bea729bdc50fc5b7b1423bd3b4
kesterlester <cgl20@cam.ac.uk>
2025-09-05 10:05:47 +0100

    Removing inappropriate test

--- MinimalConfusableSets/test_deciders.py
+++ MinimalConfusableSets/test_deciders.py
@@ -41,98 +41,99 @@ def tost_M5_k2():
     
     assert  has_True and has_False 
     
-
-def test_simple():
-        ##################################
-    import deciders
-    from sympy import Matrix
-    
-    L_matrix = Matrix([
-[-1, -1, -1, 0, 0],
-[-1,  0,  1, 0, 1]])
-    bat_matrices = [Matrix([
-[ 0, -1],
-[ 1,  3],
-[-2, -1],
-[ 1,  1],
-[ 1, -1]]), Matrix([
-[ 2, -2],
-[-1,  0],
-[ 0, -1],
-[-1,  3],
-[-1, -1]]), Matrix([
-[-1, -3],
-[ 0, -2],
-[-1,  1],
-[ 1,  0],
-[-1, -1]]), Matrix([
-[ 1,  0],
-[ 0, -2],
-[ 4,  4],
-[-1,  2],
-[ 2,  1]])]
-    decider = deciders.Rational_Decider(M=5, k=2, bat_matrices=bat_matrices)
-    
-    votes_for_collapse, null_spaces = decider.votes_for_collapse_and_null_spaces(L_matrix)
-    null_space_dimensions = tuple(len(null_space) for null_space in null_spaces)
-
-    print("vvv votes_for_collapse are: ",votes_for_collapse)
-    # Expect votes_for_collapse=(True, True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True) 
-
-    print("null space lengths are: ",null_space_dimensions)
-    # Expect null_space_dimensions=(1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) 
-    
-    print("null spaces are: ",null_spaces)
-    """
-Expect null_spaces=(
-#################
-[Matrix([
-[0],
-[0],
-[0],
-[1],
-[0]])],
-###############
-[Matrix([
-[0],
-[0],
-[0],
-[1],
-[0]])],
-###############
-[Matrix([
-[0],
-[0],
-[0],
-[1],
-[0]]), Matrix([
-[ 1/2],
-[  -1],
-[-1/2],
-[   0],
-[   1]])],
-##############
-[Matrix([
-[0],
-[0],
-[0],
-[1],
-[0]]), Matrix([
-[   1],
-[-1/2],
-[-1/4],
-[   0],
-[   1]])],
-######################
-)
-    """    
-    has_True = True in votes_for_collapse
-    has_False = True in votes_for_collapse
-
-    null_spaces_have_different_dimensions = max(null_space_dimensions) != min(null_space_dimensions)
-
-    assert not null_spaces_have_different_dimensions
-    ##################################
+#### Am commenting out the test below as I now know/realise that actually there is no reason that the null-spaces need have the same size for different bat-matrices. This test was inappropriate!
+####
+#### def test_simple():
+####         ##################################
+####     import deciders
+####     from sympy import Matrix
+####     
+####     L_matrix = Matrix([
+#### [-1, -1, -1, 0, 0],
+#### [-1,  0,  1, 0, 1]])
+####     bat_matrices = [Matrix([
+#### [ 0, -1],
+#### [ 1,  3],
+#### [-2, -1],
+#### [ 1,  1],
+#### [ 1, -1]]), Matrix([
+#### [ 2, -2],
+#### [-1,  0],
+#### [ 0, -1],
+#### [-1,  3],
+#### [-1, -1]]), Matrix([
+#### [-1, -3],
+#### [ 0, -2],
+#### [-1,  1],
+#### [ 1,  0],
+#### [-1, -1]]), Matrix([
+#### [ 1,  0],
+#### [ 0, -2],
+#### [ 4,  4],
+#### [-1,  2],
+#### [ 2,  1]])]
+####     decider = deciders.Rational_Decider(M=5, k=2, bat_matrices=bat_matrices)
+####     
+####     votes_for_collapse, null_spaces = decider.votes_for_collapse_and_null_spaces(L_matrix)
+####     null_space_dimensions = tuple(len(null_space) for null_space in null_spaces)
+#### 
+####     print("vvv votes_for_collapse are: ",votes_for_collapse)
+####     # Expect votes_for_collapse=(True, True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True) 
+#### 
+####     print("null space lengths are: ",null_space_dimensions)
+####     # Expect null_space_dimensions=(1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) 
+####     
+####     print("null spaces are: ",null_spaces)
+####     """
+#### Expect null_spaces=(
+#### #################
+#### [Matrix([
+#### [0],
+#### [0],
+#### [0],
+#### [1],
+#### [0]])],
+#### ###############
+#### [Matrix([
+#### [0],
+#### [0],
+#### [0],
+#### [1],
+#### [0]])],
+#### ###############
+#### [Matrix([
+#### [0],
+#### [0],
+#### [0],
+#### [1],
+#### [0]]), Matrix([
+#### [ 1/2],
+#### [  -1],
+#### [-1/2],
+#### [   0],
+#### [   1]])],
+#### ##############
+#### [Matrix([
+#### [0],
+#### [0],
+#### [0],
+#### [1],
+#### [0]]), Matrix([
+#### [   1],
+#### [-1/2],
+#### [-1/4],
+#### [   0],
+#### [   1]])],
+#### ######################
+#### )
+####     """    
+####     has_True = True in votes_for_collapse
+####     has_False = True in votes_for_collapse
+#### 
+####     null_spaces_have_different_dimensions = max(null_space_dimensions) != min(null_space_dimensions)
+#### 
+####     assert not null_spaces_have_different_dimensions
+####     ##################################
     
 
 def tost_worse():
