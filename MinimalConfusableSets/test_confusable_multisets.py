ff46c9630785bf917634de9400ba90650a93fe8b
kesterlester <cgl20@cam.ac.uk>
2025-09-11 14:31:57 +0100

    Fix to unit tests

--- MinimalConfusableSets/test_confusable_multisets.py
+++ MinimalConfusableSets/test_confusable_multisets.py
@@ -25,7 +25,7 @@ def test():
 def test2():
 
     scaled_bat_matrix = sp.Matrix([[-2, -1], [0, -1], [1, 1], [-2, 1], [-1, 3], [1, -1]])
-    E, O, C, EE, OO = confusable_multisets.analyze_B(scaled_bat_matrix, plot_if_2d=False, show_C_if_plotting = False)
+    E, O, C, EE, OO = confusable_multisets.mitm_compute_E_O_C_EE_OO(scaled_bat_matrix)
 
     assert EE.total() == OO.total()
     assert len(EE) != len(OO) # This result might be surprising until you realise that EE and OO are objects of type collection.Counter !
